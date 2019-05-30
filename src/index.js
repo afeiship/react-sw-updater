988a6bb406a80d433f47b647c6ca971d838d4f6f
afeiship <1290657123@qq.com>
2019-05-30 23:00:42 +0800

    add: basic rich-text example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app';
-import CheckLists from './check-lists'
-render(<CheckLists />, document.getElementById('app'));
+import CheckLists from './check-lists';
+import RichText from './rich-text';
+render(<RichText />, document.getElementById('app'));
