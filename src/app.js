15b0c292bde3d758569b595c5f8f21c32ea82732
afeiship <1290657123@qq.com>
2019-05-30 21:55:27 +0800

    add: code toggle logic

--- src/app.js
+++ src/app.js
@@ -1,55 +1,69 @@
 import React from 'react';
 import { Editor } from 'slate-react';
 import { Value } from 'slate';
+import data from './assets/value';
 
 // 构建初始状态…
-const initialState = Value.fromJSON({
-  document: {
-    nodes: [
-      {
-        object: 'block',
-        type: 'paragraph',
-        nodes: [
-          {
-            object: 'text',
-            leaves: [
-              {
-                text: 'A line of text in a paragraph.'
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-});
+const initialState = Value.fromJSON(data);
+
+function CodeNode(inProps) {
+  const { attributes, children } = inProps;
+  return (
+    <pre {...attributes}>
+      <code>{children}</code>
+    </pre>
+  );
+}
 
 export default class extends React.Component {
   state = {
     value: initialState
   };
 
-  constructor(inProps){
+  constructor(inProps) {
     super(inProps);
     this.editor = React.createRef();
   }
 
-  componentDidMount() {
-    window.editor = this.editor.current;
-  }
-
   _onChange = (inEvent) => {
     const { value } = inEvent;
-    console.log('value:->', value);
-    console.log(value.toJSON());
+    // console.log('value:->', value);
+    // console.log(value.toJSON());
     this.setState({ value });
+  };
+
+  _onKeyDown = (inEvent, inEditor, inNext) => {
+    if (inEvent.ctrlKey && inEvent.key === '`') {
+      inEvent.preventDefault();
+      const isCode = inEditor.value.blocks.some(
+        (block) => block.type == 'code'
+      );
+      // toggle logic:
+      return inEditor.setBlocks(isCode ? 'paragraph' : 'code');
+    }
+    return inNext();
+  };
+
+  _onRenderBlock = (inProps, inEditor, inNext) => {
+    switch (inProps.node.type) {
+      case 'code':
+        return <CodeNode {...inProps} />;
+      default:
+        return inNext();
+    }
   };
 
   render() {
     const { value } = this.state;
     return (
       <div>
-        <Editor ref={this.editor} value={value} onChange={this._onChange} />
+        <Editor
+          ref={this.editor}
+          value={value}
+          renderBlock={this._onRenderBlock}
+          onChange={this._onChange}
+          onKeyDown={this._onKeyDown}
+        />
       </div>
     );
   }
