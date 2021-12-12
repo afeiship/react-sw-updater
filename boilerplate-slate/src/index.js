deca9c521519c2d6e123086b667a5e1a8bf517cf
afeiship <1290657123@qq.com>
2021-12-12 19:28:10 +0800

    feat: leaf

--- boilerplate-slate/src/index.js
+++ boilerplate-slate/src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom';
 import React, { useMemo, useState } from 'react';
-import { createEditor } from 'slate';
+import { Editor, Transforms, createEditor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
+import { DefaultElement, CodeElement } from './elements';
 
 const App = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
@@ -12,14 +13,13 @@ const App = () => {
     }
   ]);
 
-  const DefaultElement = (props) => {
-    return <p className='i-am-p' {...props.attributes}>{props.children}</p>;
-  };
 
-// Define a rendering function based on the element passed to `props`. We use
-// `useCallback` here to memoize the function for subsequent renders.
+  // Define a rendering function based on the element passed to `props`. We use
+  // `useCallback` here to memoize the function for subsequent renders.
   const renderElement = (props) => {
     switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />;
       default:
         return <DefaultElement {...props} />;
     }
@@ -41,11 +41,21 @@ const App = () => {
         renderElement={renderElement}
         onKeyDown={event => {
           console.log('keydown: ', event);
-          if (event.key === '&') {
-            // Prevent the ampersand character from being inserted.
+          if (event.key === '`' && event.ctrlKey) {
+            // Prevent the "`" from being inserted by default.
             event.preventDefault();
-            // Execute the `insertText` method when the event occurs.
-            editor.insertText('and');
+            event.preventDefault();
+            // Determine whether any of the currently selected blocks are code blocks.
+            const [match] = Editor.nodes(editor, {
+              match: n => n.type === 'code'
+            });
+            console.log('match:', );
+            // Toggle the block type depending on whether there's already a match.
+            Transforms.setNodes(
+              editor,
+              { type: match ? 'paragraph' : 'code' },
+              // { match: n => Editor.isBlock(editor, n) }
+            );
           }
         }}
       />
